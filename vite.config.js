cd5f2eb2354d213bb07c5ce2cb367e276c3b63c2
Leonardo Carrozzo <leonardomaria.c@gmail.com>
2023-07-16 17:02:31 +0200

    Deleted unused models/textures

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 // vite.config.js
 export default {
     // config options
-    base: '/SapienzaInteractiveGraphicsCourse/final-project-zombies-attack/'
+    base: '/final-project-zombies-attack/'
 }
